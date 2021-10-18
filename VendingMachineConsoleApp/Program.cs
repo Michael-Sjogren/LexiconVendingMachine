e254650d2d978279699468766cc6de318de37914
michael <carl.michael.sjogren@gmail.com>
2021-10-18 19:15:25 +0200

    Basic console program

---
+++ VendingMachineConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using VendingMachineLib;
+
+namespace VendingMachineConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vendor = new VendingMachine(CurrencyType.SEK, new uint[] {1, 5, 20, 50, 100, 500, 1000});
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
